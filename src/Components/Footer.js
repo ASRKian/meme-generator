67c5c0459f7c45afdb90d359e83d50dde962ae07
Lokendra Gupta <guptalokendra18@gmail.com>
2023-05-12 22:33:34 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,6 +3,6 @@ import '../App.css'
 
 export default function Footer() {
   return (
-    <footer className="footer">©️ Quote Web App. All right reserved by <span className="name">LOKENDRA GUPTA</span></footer>
+    <footer className="footer">©️ Meme Generator App. All right reserved by <span className="name">LOKENDRA GUPTA</span></footer>
   )
 }
