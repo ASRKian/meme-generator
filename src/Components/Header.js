c18b8cee6cc95d2f95801e206b880b066543ffe1
Lokendra Gupta <guptalokendra18@gmail.com>
2023-05-12 22:18:26 +0530

    Initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../App.css'
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <h2 className="heading">Meme Generator App</h2>
+    </div>
+  )
+}
